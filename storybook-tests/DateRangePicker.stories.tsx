45691f1af05d2919df2b3b5dc3941f88bc110830
chrisgarber <30669660+chrisgarber@users.noreply.github.com>
2025-01-06 14:40:34 -0800

    fix: better formatting in DateRangePicker tests

--- storybook-tests/DateRangePicker.stories.tsx
+++ storybook-tests/DateRangePicker.stories.tsx
@@ -10,9 +10,9 @@ interface DateRange {
 }
 
 type DateRangePickerProps = {
-  onChange: (ranges: {selection: DateRange}) => void;
+  onChange: (ranges: { selection: DateRange }) => void;
   ranges: DateRange[];
-}
+};
 
 const stateDecorator = controlledDecorator<DateRangePickerProps>(
   "onChange",
@@ -25,7 +25,13 @@ export default {
     layout: "centered",
   },
   args: {
-    ranges: [{ startDate: new Date(1714383846000), endDate: new Date(1714483846000), key: "selection" }],
+    ranges: [
+      {
+        startDate: new Date(1714383846000),
+        endDate: new Date(1714483846000),
+        key: "selection",
+      },
+    ],
     showSelectionPreview: true,
     moveRangeOnFirstSelection: false,
     months: 2,
